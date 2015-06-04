99e8ab4a0fc469e904dd82f46f8d8ea45fdf51c8
Dimitri König <dimitri.koenig@unic.com>
2015-06-04 13:18:31 +0200

    Initial working version

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'username' => 'your.username',
+	'password' => 'your.password',
+	'hostUrl' => 'https://your.confluence.host.url'
+);
+
+?>
---
+++ workflow.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once('workflows-library.php');
+
+$config = (require_once 'config.php');
+$options = array(
+	CURLOPT_USERPWD => $config['username'] . ':' . $config['password']
+);
+
+try {
+	$wf = new Workflows();
+
+	$response = $wf->request($config['hostUrl'] . '/rest/searchv3/latest/search?queryString=' . urlencode($input), $options);
+	$jsonResponse = json_decode($response);
+
+	if ($jsonResponse->errorMessages) {
+		foreach ($jsonResponse->errorMessages as $errorMessage) {
+			$wf->result('confluence-response-error', $input, 'Error message', $errorMessage, 'icon.png');
+		}
+	}
+
+	if ($jsonResponse->total === 0) {
+		$wf->result('confluence-no-results', $input, 'No Suggestions', 'No search suggestions for "' . $input . '" found', 'icon.png');
+	}
+
+	if ($jsonResponse->total > 0) {
+		foreach ($jsonResponse->results as $result) {
+			$wf->result('confluence-' . $result->id, $config['hostUrl'] . $result->url, removeHighlight($result->title), $result->friendlyDate . ' | ' . removeHighlight($result->bodyTextHighlights), '');
+		}
+	}
+} catch (Exception $e) {
+	$wf->result('confluence-request-error', $input, 'Search Request Error', 'Error when searching for "' . $searchWords, 'icon.png');
+}
+
+echo $wf->toxml();
+
+
+function removeHighlight($input) {
+	return preg_replace('/@@@[^@]+@@@(.*)@@@[^@]+@@@/Uis', '$1', $input);
+}
+
+?>
