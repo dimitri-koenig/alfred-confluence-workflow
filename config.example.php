c798f7316f792e25770b1ae6fe6e237959d9d477
Dimitri König <dimitri@dimitrikoenig.net>
2015-06-05 14:13:17 +0200

    Changed default credentials fetching to mac keychain

--- config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	'username' => 'your.username',
-	'password' => 'your.password',
-	'hostUrl' => 'https://your.confluence.host.url'
-);
-
-?>
--- workflow.php
+++ workflow.php
@@ -3,6 +3,33 @@
 require_once('workflows-library.php');
 
 $config = (require_once 'config.php');
+
+if ($config['useLocalKeychain']) {
+	exec('security find-internet-password -j "' . $_ENV['alfred_workflow_bundleid'] . '" -g 2>&1; echo $?', $keychainData);
+
+	$protocol = '';
+	$server = '';
+	foreach ($keychainData as $singleLine) {
+		if (stripos($singleLine, '"acct"') !== FALSE) {
+			$config['username'] = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
+			continue;
+		}
+		if (stripos($singleLine, 'password:') !== FALSE) {
+			$config['password'] = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
+			continue;
+		}
+		if (stripos($singleLine, '"ptcl"') !== FALSE) {
+			$protocol = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
+			continue;
+		}
+		if (stripos($singleLine, '"srvr"') !== FALSE) {
+			$server = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
+			continue;
+		}
+	}
+	$config['hostUrl'] = ($protocol === 'htps' ? 'https://' : 'http://') . $server;
+}
+
 $options = array(
 	CURLOPT_USERPWD => $config['username'] . ':' . $config['password']
 );
