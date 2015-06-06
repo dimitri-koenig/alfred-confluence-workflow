f0e7b331810bb0edd25128b0950c6cccd9dfdceb
Dimitri König <dimitri@dimitrikoenig.net>
2015-06-06 18:35:40 +0200

    Code Style Refactoring

--- workflow.php
+++ workflow.php
@@ -4,25 +4,31 @@ require_once('workflows-library.php');
 
 $config = (require_once 'config.php');
 
-if ($config['useLocalKeychain']) {
+if ($config['useLocalKeychain'])
+{
 	exec('security find-internet-password -j "' . $_ENV['alfred_workflow_bundleid'] . '" -g 2>&1; echo $?', $keychainData);
 
 	$protocol = '';
 	$server = '';
-	foreach ($keychainData as $singleLine) {
-		if (stripos($singleLine, '"acct"') !== FALSE) {
+	foreach ($keychainData as $singleLine)
+	{
+		if (stripos($singleLine, '"acct"') !== FALSE)
+		{
 			$config['username'] = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
 			continue;
 		}
-		if (stripos($singleLine, 'password:') !== FALSE) {
+		if (stripos($singleLine, 'password:') !== FALSE)
+		{
 			$config['password'] = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
 			continue;
 		}
-		if (stripos($singleLine, '"ptcl"') !== FALSE) {
+		if (stripos($singleLine, '"ptcl"') !== FALSE)
+		{
 			$protocol = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
 			continue;
 		}
-		if (stripos($singleLine, '"srvr"') !== FALSE) {
+		if (stripos($singleLine, '"srvr"') !== FALSE)
+		{
 			$server = preg_replace('/^.*"([^"]+)"\w*$/', '$1', $singleLine);
 			continue;
 		}
@@ -30,40 +36,46 @@ if ($config['useLocalKeychain']) {
 	$config['hostUrl'] = ($protocol === 'htps' ? 'https://' : 'http://') . $server;
 }
 
-$options = array(
+$options = [
 	CURLOPT_USERPWD => $config['username'] . ':' . $config['password']
-);
+];
 
-try {
+try
+{
 	$wf = new Workflows();
 
 	$response = $wf->request($config['hostUrl'] . '/rest/searchv3/latest/search?queryString=' . urlencode($input), $options);
 	$jsonResponse = json_decode($response);
 
-	if ($jsonResponse->errorMessages) {
-		foreach ($jsonResponse->errorMessages as $errorMessage) {
+	if ($jsonResponse->errorMessages)
+	{
+		foreach ($jsonResponse->errorMessages as $errorMessage)
+		{
 			$wf->result('confluence-response-error', $input, 'Error message', $errorMessage, 'icon.png');
 		}
 	}
 
-	if ($jsonResponse->total === 0) {
+	if ($jsonResponse->total === 0)
+	{
 		$wf->result('confluence-no-results', $input, 'No Suggestions', 'No search suggestions for "' . $input . '" found', 'icon.png');
 	}
 
-	if ($jsonResponse->total > 0) {
-		foreach ($jsonResponse->results as $result) {
+	if ($jsonResponse->total > 0)
+	{
+		foreach ($jsonResponse->results as $result)
+		{
 			$wf->result('confluence-' . $result->id, $config['hostUrl'] . $result->url, removeHighlight($result->title), $result->friendlyDate . ' | ' . removeHighlight($result->bodyTextHighlights), '');
 		}
 	}
-} catch (Exception $e) {
+}
+catch (Exception $e)
+{
 	$wf->result('confluence-request-error', $input, 'Search Request Error', 'Error when searching for "' . $searchWords, 'icon.png');
 }
 
 echo $wf->toxml();
 
-
-function removeHighlight($input) {
+function removeHighlight($input)
+{
 	return preg_replace('/@@@[^@]+@@@(.*)@@@[^@]+@@@/Uis', '$1', $input);
 }
-
-?>
