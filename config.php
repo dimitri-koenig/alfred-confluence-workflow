7bf086b85892f0cdb3e77b518cfb86c9ec1b4797
Dimitri König <dimitri@dimitrikoenig.net>
2015-06-06 18:35:40 +0200

    Code Style Refactoring

--- config.php
+++ config.php
@@ -1,10 +1,8 @@
 <?php
 
-return array(
+return [
 	'useLocalKeychain' => true, // set to false if you want to setup below config data
-	'username' => '', // your confluence username
-	'password' => '', // your confluence password
-	'hostUrl' => '' // your confluence url, ex: https://my.confluence.com
-);
-
-?>
+	'username'         => '', // your confluence username
+	'password'         => '', // your confluence password
+	'hostUrl'          => '' // your confluence url, ex: https://my.confluence.com
+];
