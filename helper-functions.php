56e829381781b30ac5d0aee125ca08632b2b2019
Dimitri König <dimitri@dimitrikoenig.net>
2015-06-17 15:36:55 +0200

    Downgraded scripts to 5.3 comp

--- helper-functions.php
+++ helper-functions.php
@@ -2,34 +2,34 @@
 
 function getBundleId()
 {
-	return isset($_ENV['alfred_workflow_bundleid']) ? $_ENV['alfred_workflow_bundleid'] : $_SERVER['alfred_workflow_bundleid'];
+    return isset($_ENV['alfred_workflow_bundleid']) ? $_ENV['alfred_workflow_bundleid'] : $_SERVER['alfred_workflow_bundleid'];
 }
 
 function getCredentialsFromLocalKeychain()
 {
-	$keychainData = shell_exec('security find-internet-password -j "' . getBundleId() . '" -g 2>&1; echo $?');
+    $keychainData = shell_exec('security find-internet-password -j "' . getBundleId() . '" -g 2>&1; echo $?');
 
-	$protocol = findValue('/"ptcl".*"([^"]+)"\W*/Uis', $keychainData);
-	$server = findValue('/"srvr".*"([^"]+)"\W*/Uis', $keychainData);
+    $protocol = findValue('/"ptcl".*"([^"]+)"\W*/Uis', $keychainData);
+    $server = findValue('/"srvr".*"([^"]+)"\W*/Uis', $keychainData);
 
-	$config = [
-		'hostUrl'  => ($protocol === 'htps' ? 'https://' : 'http://') . $server,
-		'username' => findValue('/"acct".*"([^"]+)"\W*/Uis', $keychainData),
-		'password' => findValue('/password:\W*"([^"]+)"/', $keychainData)
-	];
+    $config = array(
+        'hostUrl'  => ($protocol === 'htps' ? 'https://' : 'http://') . $server,
+        'username' => findValue('/"acct".*"([^"]+)"\W*/Uis', $keychainData),
+        'password' => findValue('/password:\W*"([^"]+)"/', $keychainData)
+    );
 
-	return $config;
+    return $config;
 }
 
 function findValue($pattern, $haystack)
 {
-	$matches = [];
-	preg_match($pattern, $haystack, $matches);
+    $matches = array();
+    preg_match($pattern, $haystack, $matches);
 
-	return count($matches) ? trim(array_pop($matches)) : null;
+    return count($matches) ? trim(array_pop($matches)) : null;
 }
 
 function removeHighlight($input)
 {
-	return preg_replace('/@@@[^@]+@@@(.*)@@@[^@]+@@@/Uis', '$1', $input);
+    return preg_replace('/@@@[^@]+@@@(.*)@@@[^@]+@@@/Uis', '$1', $input);
 }
