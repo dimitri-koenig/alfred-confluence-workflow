b750877c10e3e6ba8342a4febfa45d309ffcbc56
Dimitri König <dimitri@dimitrikoenig.net>
2015-06-17 15:36:55 +0200

    Downgraded scripts to 5.3 comp

--- workflow.php
+++ workflow.php
@@ -3,48 +3,45 @@
 require_once('workflows-library.php');
 require_once('helper-functions.php');
 
+$wf = new Workflows();
+
 $config = (require_once 'config.php');
 
-if ($config['useLocalKeychain'])
-{
-	$config = array_merge($config, getCredentialsFromLocalKeychain());
+if ($config['useLocalKeychain']) {
+    $config = array_merge($config, getCredentialsFromLocalKeychain());
 }
 
-$options = [
-	CURLOPT_USERPWD => $config['username'] . ':' . $config['password']
-];
-
-try
-{
-	$wf = new Workflows();
-
-	$response = $wf->request($config['hostUrl'] . '/rest/searchv3/latest/search?queryString=' . urlencode($input), $options);
-	$jsonResponse = json_decode($response);
-
-	if (isset($jsonResponse->errorMessages))
-	{
-		foreach ($jsonResponse->errorMessages as $errorMessage)
-		{
-			$wf->result('confluence-response-error', $input, 'Error message', $errorMessage, 'icon.png');
-		}
-	}
-
-	if ($jsonResponse->total === 0)
-	{
-		$wf->result('confluence-no-results', $input, 'No Suggestions', 'No search suggestions for "' . $input . '" found', 'icon.png');
-	}
-
-	if ($jsonResponse->total > 0)
-	{
-		foreach ($jsonResponse->results as $result)
-		{
-			$wf->result('confluence-' . $result->id, $config['hostUrl'] . $result->url, removeHighlight($result->title), $result->friendlyDate . ' | ' . removeHighlight($result->bodyTextHighlights), '');
-		}
-	}
+if (empty($config['username']) || empty($config['password'])) {
+    $wf->result('confluence-auth-error', '', 'Auth config incomplete', '', 'icon.png');
+    echo $wf->toxml();
+    die();
 }
-catch (Exception $e)
-{
-	$wf->result('confluence-request-error', $input, 'Search Request Error', 'Error when searching for "' . $searchWords, 'icon.png');
+
+$options = array(
+    CURLOPT_USERPWD => $config['username'] . ':' . $config['password']
+);
+
+try {
+    $response = $wf->request($config['hostUrl'] . '/rest/searchv3/latest/search?queryString=' . urlencode($input), $options);
+    $jsonResponse = json_decode($response);
+
+    if (isset($jsonResponse->errorMessages)) {
+        foreach ($jsonResponse->errorMessages as $errorMessage) {
+            $wf->result('confluence-response-error', $input, 'Error message', $errorMessage, 'icon.png');
+        }
+    }
+
+    if ($jsonResponse->total === 0) {
+        $wf->result('confluence-no-results', $input, 'No Suggestions', 'No search suggestions for "' . $input . '" found', 'icon.png');
+    }
+
+    if ($jsonResponse->total > 0) {
+        foreach ($jsonResponse->results as $result) {
+            $wf->result('confluence-' . $result->id, $config['hostUrl'] . $result->url, removeHighlight($result->title), $result->friendlyDate . ' | ' . removeHighlight($result->bodyTextHighlights), '');
+        }
+    }
+} catch (Exception $e) {
+    $wf->result('confluence-request-error', $input, 'Search Request Error', 'Error when searching for "' . $searchWords, 'icon.png');
 }
 
 echo $wf->toxml();
